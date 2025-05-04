08d16f6f816a1d6c723b4ee3af0242e4cea30228
Flavius Marian <marianflavius27@yahoo.com>
2025-05-04 20:29:51 +0300

    Modificare main + alu pt adaugarea pt carry in si out + rest

--- ALU/ALU_mux.h
+++ ALU/ALU_mux.h
@@ -1,3 +1,4 @@
+// ALU_mux.h
 #ifndef ALU_MUX_H
 #define ALU_MUX_H
 
@@ -13,31 +14,29 @@
 #include "Mux8_32.h"
 
 SC_MODULE(ALU) {
-    // Port-uri externe
-    sc_in<sc_int<32>>   A, B;
-    sc_in<sc_uint<3>>   opcode;
-    sc_out<sc_int<32>>  result;
+    // External ports
+    sc_in<sc_int<32>>    A, B;
+    sc_in<sc_uint<3>>    opcode;
+    sc_in<bool>          Cin;
+    sc_out<sc_int<32>>   result;
+    sc_out<bool>         CoutAdd;
+    sc_out<bool>         CoutSub;
+    sc_out<sc_int<32>>   Remainder;
 
-    // Semnale interne pe 32 biți
-    sc_signal<sc_int<32>>   res_add, res_sub, res_mul, res_div;
-    sc_signal<sc_int<32>>   res_and, res_or, res_xor, res_not;
+    // Internal signals
+    sc_signal<sc_int<32>> res_add, res_sub, res_mul, res_div;
+    sc_signal<sc_int<32>> res_and, res_or, res_xor, res_not;
 
-    // Semnale dummy pentru carry/borrow
-    sc_signal<bool>         dummy_cin;
-    sc_signal<bool>         dummy_cout_add;
-    sc_signal<bool>         dummy_cout_sub;
+    // Adaptation signals
+    sc_signal<sc_int<64>>   full_mul;
+    sc_signal<sc_int<16>>   A16, B16, div_q16, div_r16;
+    sc_signal<sc_uint<32>>  A_u, B_u, and_u, or_u, xor_u, not_u;
 
-    // Semnale intermediare pentru adaptări
-    sc_signal<sc_int<64>>   full_mul;                // produs 64 biți
-    sc_signal<sc_int<16>>   A16, B16, div_q16, div_r16; // div inputs/outputs 16 biți
-    sc_signal<sc_uint<32>>  A_u, B_u;                // A, B pentru bitwise
-    sc_signal<sc_uint<32>>  and_u, or_u, xor_u, not_u; // rezultate bitwise unsigned
-
-    // Module interne
+    // Submodules
     RCA32                   adder;
     Subtractor32            subtractor;
     BoothMultiplier<32>     multiplier;
-    SRT2Divider             divider;  // modul original, pe 16 biți
+    SRT2Divider             divider;
     BitwiseAnd<32>          and_mod;
     BitwiseOr<32>           or_mod;
     BitwiseXor<32>          xor_mod;
@@ -49,25 +48,26 @@ SC_MODULE(ALU) {
         divider("divider"), and_mod("and_mod"), or_mod("or_mod"),
         xor_mod("xor_mod"), not_mod("not_mod"), mux("mux")
     {
-        // Adaptări intermediare
-        SC_METHOD(slice_mul);         sensitive << full_mul;
-        SC_METHOD(slice_div_inputs);  sensitive << A << B;
-        SC_METHOD(extend_div);        sensitive << div_q16;
-        SC_METHOD(cast_bitwise_inputs); sensitive << A << B;
-        SC_METHOD(extend_bitwise);    sensitive << and_u << or_u << xor_u << not_u;
+        // Processes for adaptation
+        SC_METHOD(slice_mul);        sensitive << full_mul;
+        SC_METHOD(slice_div_inputs); sensitive << A << B;
+        SC_METHOD(extend_div);       sensitive << div_q16;
+        SC_METHOD(extend_rem);       sensitive << div_r16;
+        SC_METHOD(cast_bitwise);     sensitive << A << B;
+        SC_METHOD(extend_bitwise);   sensitive << and_u << or_u << xor_u << not_u;
 
         // ADD
         adder.A(A);
         adder.B(B);
-        adder.Cin(dummy_cin);
+        adder.Cin(Cin);
         adder.S(res_add);
-        adder.Cout(dummy_cout_add);
+        adder.Cout(CoutAdd);
 
         // SUB
         subtractor.A(A);
         subtractor.B(B);
         subtractor.S(res_sub);
-        subtractor.Cout(dummy_cout_sub);
+        subtractor.Cout(CoutSub);
 
         // MUL
         multiplier.multiplicand(A);
@@ -81,19 +81,12 @@ SC_MODULE(ALU) {
         divider.remainder(div_r16);
 
         // BITWISE
-        and_mod.A(A_u);
-        and_mod.B(B_u);
-        and_mod.Y(and_u);
-        or_mod.A(A_u);
-        or_mod.B(B_u);
-        or_mod.Y(or_u);
-        xor_mod.A(A_u);
-        xor_mod.B(B_u);
-        xor_mod.Y(xor_u);
-        not_mod.A(A_u);
-        not_mod.Y(not_u);
+        and_mod.A(A_u); and_mod.B(B_u); and_mod.Y(and_u);
+        or_mod.A(A_u);  or_mod.B(B_u);  or_mod.Y(or_u);
+        xor_mod.A(A_u); xor_mod.B(B_u); xor_mod.Y(xor_u);
+        not_mod.A(A_u);               not_mod.Y(not_u);
 
-        // MUX 8:1
+        // MUX
         mux.d0(res_add);
         mux.d1(res_sub);
         mux.d2(res_mul);
@@ -107,23 +100,22 @@ SC_MODULE(ALU) {
     }
 
     void slice_mul() {
-        res_mul.write(full_mul.read().range(31, 0));
+        res_mul.write(full_mul.read().range(31,0));
     }
-
     void slice_div_inputs() {
-        A16.write(A.read().range(15, 0));
-        B16.write(B.read().range(15, 0));
+        A16.write(A.read().range(15,0));
+        B16.write(B.read().range(15,0));
     }
-
     void extend_div() {
         res_div.write((sc_int<32>)div_q16.read());
     }
-
-    void cast_bitwise_inputs() {
+    void extend_rem() {
+        Remainder.write((sc_int<32>)div_r16.read());
+    }
+    void cast_bitwise() {
         A_u.write((sc_uint<32>)A.read());
         B_u.write((sc_uint<32>)B.read());
     }
-
     void extend_bitwise() {
         res_and.write((sc_int<32>)and_u.read());
         res_or.write((sc_int<32>)or_u.read());
--- ALU/main.cpp
+++ ALU/main.cpp
@@ -2,108 +2,106 @@
 #include "ALU_mux.h"
 
 int sc_main(int argc, char* argv[]) {
-     // Semnale pentru intrări și ieșire
-    sc_signal<sc_int<32>> A, B, result;
-    sc_signal<sc_uint<3>> opcode;
+     sc_signal<sc_int<32>> A, B, result, rem;
+     sc_signal<sc_uint<3>> opcode;
+     sc_signal<bool> Cin, CoutAdd, CoutSub;
 
-    // Instanțiem ALU
-    ALU alu("alu");
-    alu.A(A);
-    alu.B(B);
-    alu.opcode(opcode);
-    alu.result(result);
+     ALU alu("alu");
+     alu.A(A);          alu.B(B);
+     alu.opcode(opcode);
+     alu.Cin(Cin);
+     alu.result(result);
+     alu.CoutAdd(CoutAdd);
+     alu.CoutSub(CoutSub);
+     alu.Remainder(rem);
 
-    //=== ADD ===
-    cout << "\n=== ADD ===" << endl;
-    A.write(648); B.write(4); opcode.write(0);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== ADD ===
+     cout << "\n=== ADD ===" << endl;
+     A.write(648); B.write(4); Cin.write(false); opcode.write(0);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " Cin=" << Cin.read()
+          << " => S=" << result.read()
+          << " Cout=" << CoutAdd.read()
+          << endl;
+     A.write(5); B.write(6); Cin.write(true); opcode.write(0);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " Cin=" << Cin.read()
+          << " => S=" << result.read()
+          << " Cout=" << CoutAdd.read()
+          << endl;
 
-    A.write(5); B.write(6); opcode.write(0);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== SUBTRACT ===
+     cout << "\n=== SUBTRACT ===" << endl;
+     A.write(20); B.write(5); Cin.write(false); opcode.write(1);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " => S=" << result.read()
+          << " Cout=" << CoutSub.read()
+          << endl;
+     A.write(45); B.write(15); Cin.write(false); opcode.write(1);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " => S=" << result.read()
+          << " Cout=" << CoutSub.read()
+          << endl;
 
-    //=== SUBTRACT ===
-    cout << "\n=== SUBTRACT ===" << endl;
-    A.write(20); B.write(5); opcode.write(1);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== MULTIPLY ===
+     cout << "\n=== MULTIPLY (Booth) ===" << endl;
+     A.write(7); B.write(-3); opcode.write(2);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " * B=" << B.read()
+          << " => S=" << result.read() << endl;
+     A.write(-8); B.write(-4); opcode.write(2);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " * B=" << B.read()
+          << " => S=" << result.read() << endl;
+     A.write(7); B.write(8); opcode.write(2);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " * B=" << B.read()
+          << " => S=" << result.read() << endl;
 
-    A.write(45); B.write(15); opcode.write(1);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== DIVIDE ===
+     cout << "\n=== DIVIDE ===" << endl;
+     A.write(1234); B.write(56); opcode.write(3);
+     sc_start(1, SC_NS);
+     cout << "Dividend=" << A.read() << " Divisor=" << B.read()
+          << " => Quotient=" << result.read()
+          << " Remainder=" << rem.read() << endl;
+     A.write(-500); B.write(13); opcode.write(3);
+     sc_start(1, SC_NS);
+     cout << "Dividend=" << A.read() << " Divisor=" << B.read()
+          << " => Quotient=" << result.read()
+          << " Remainder=" << rem.read() << endl;
 
-    //=== MULTIPLY ===
-    cout << "\n=== MULTIPLY (Booth) ===" << endl;
-    A.write(7); B.write(-3); opcode.write(2);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " * B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== BITWISE AND ===
+     cout << "\n=== BITWISE AND ===" << endl;
+     A.write(13); B.write(10); opcode.write(4);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " => S=" << result.read() << endl;
 
-    //=== DIVIDE ===
-    cout << "\n=== DIVIDE (SRT2) ===" << endl;
-    A.write(1234); B.write(56); opcode.write(3);
-    sc_start(1, SC_NS);
-    cout << "Dividend=" << A.read()
-         << " Divisor="  << B.read()
-         << " => Quotient=" << result.read()
-         << endl;
+     //=== BITWISE OR ===
+     cout << "\n=== BITWISE OR ===" << endl;
+     A.write(13); B.write(10); opcode.write(5);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " => S=" << result.read() << endl;
 
-    A.write(-500); B.write(13); opcode.write(3);
-    sc_start(1, SC_NS);
-    cout << "Dividend=" << A.read()
-         << " Divisor="  << B.read()
-         << " => Quotient=" << result.read()
-         << endl;
+     //=== BITWISE XOR ===
+     cout << "\n=== BITWISE XOR ===" << endl;
+     A.write(13); B.write(10); opcode.write(6);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read() << " B=" << B.read()
+          << " => S=" << result.read() << endl;
 
-    //=== AND ===
-    cout << "\n=== BITWISE AND ===" << endl;
-    A.write(13); B.write(10); opcode.write(4);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
+     //=== BITWISE NOT ===
+     cout << "\n=== BITWISE NOT ===" << endl;
+     A.write(13); B.write(0); opcode.write(7);
+     sc_start(1, SC_NS);
+     cout << "A=" << A.read()
+          << " => S=" << result.read() << endl;
 
-    //=== OR ===
-    cout << "\n=== BITWISE OR ===" << endl;
-    A.write(13); B.write(10); opcode.write(5);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
-
-    //=== XOR ===
-    cout << "\n=== BITWISE XOR ===" << endl;
-    A.write(13); B.write(10); opcode.write(6);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " B=" << B.read()
-         << " => Result=" << result.read()
-         << endl;
-
-    //=== NOT ===
-    cout << "\n=== BITWISE NOT ===" << endl;
-    A.write(13); B.write(0); opcode.write(7);
-    sc_start(1, SC_NS);
-    cout << "A=" << A.read()
-         << " => Result=" << result.read()
-         << endl;
-
-    return 0;
+     return 0;
 }
